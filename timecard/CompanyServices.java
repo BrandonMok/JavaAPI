6cbff5977fdc8ea6f60ff12f344b0b3c9b34084f
BrandonMok <brandonmok88@yahoo.com>
2019-11-03 12:02:26 -0500

    Getting an employee (/employee?company={}&emp_id={}) added

--- timecard/CompanyServices.java
+++ timecard/CompanyServices.java
@@ -16,9 +16,6 @@ public class CompanyServices {
    // Data Layer
    DataLayer dl = null;
    
-   // My ID
-   String id = "bxm5989";
-   
    // Business layer - validation
    BusinessLayer bl = new BusinessLayer();
     
@@ -31,7 +28,7 @@ public class CompanyServices {
    @Produces("application/json")
    public Response getAllDepartment(@QueryParam("company") String company){
       try {
-         dl = new DataLayer(id);
+         dl = new DataLayer(company);
          
          // Get all departments from datalayer
          List<Department> departmentsList = dl.getAllDepartment(company);
@@ -60,7 +57,7 @@ public class CompanyServices {
       @QueryParam("dept_id") int dept_id
    ) {
       try{
-         dl = new DataLayer(id);      
+         dl = new DataLayer(company);      
          
          Department dep = dl.getDepartment(company, dept_id);
     
@@ -89,7 +86,7 @@ public class CompanyServices {
    @Produces("application/json")
    public Response getAllEmployee(@QueryParam("company") String company){
       try{
-         dl = new DataLayer(id);
+         dl = new DataLayer(company);
          
           // Get all departments from datalayer
          List<Employee> employeeList = dl.getAllEmployee(company);
@@ -108,7 +105,34 @@ public class CompanyServices {
          dl.close();
       }
    }
-     
+   
+   // localhost:8080/MokBP2/resources/CompanyServices/employee?company={company}&emp_id={emp_id}
+   @Path("employee")
+   @GET
+   @Produces("application/json")
+   public Response getEmployee(
+      @QueryParam("company") String company,
+      @QueryParam("emp_id") int emp_id 
+   ){
+      try{
+         dl = new DataLayer(company);
+         
+         Employee emp = dl.getEmployee(emp_id);
+         if(emp != null){
+              String employee = bl.employeeToJSON(emp);
+              return Response.ok("{\"success\":" + employee + "}").build();
+          }
+          else {
+             return bl.notFound("Employee not found with ID: ", String.valueOf(emp_id)).build();
+          }       
+      }
+      catch(Exception e){
+         return bl.error(e).build();
+      }
+      finally{
+         dl.close();
+      }
+   }     
    
      
    
